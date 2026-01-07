5408e031c938356e4e328ca34fba214a98a64ed8
princehw03 <princehw03@gmail.com>
2026-01-07 15:48:51 +0900

    Feat: DiaryFragment와 DiaryWriteFragment간의 전환 로직 추가

--- app/src/main/java/com/example/egobook_frontent/ui/diary/DiaryFragment.kt
+++ app/src/main/java/com/example/egobook_frontent/ui/diary/DiaryFragment.kt
@@ -5,20 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.egobook_frontent.R
 import com.example.egobook_frontent.databinding.FragmentDiaryBinding
 
 class DiaryFragment : Fragment() {
-    lateinit var binding: FragmentDiaryBinding
-    private var diaryDatas = ArrayList<Diary>()
+    private var _binding: FragmentDiaryBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        binding = FragmentDiaryBinding.inflate(inflater, container, false)
-
+    ): View {
+        _binding = FragmentDiaryBinding.inflate(inflater, container, false)
         return binding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // 일기 추가 버튼 클릭 리스너
+        binding.btnAdd.setOnClickListener {
+            findNavController().navigate(R.id.action_diaryFragment_to_diaryWriteFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
