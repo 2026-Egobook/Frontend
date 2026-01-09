28b962e77752f95b9d2dd1af3e8ed46e64d4035f
princehw03 <princehw03@gmail.com>
2026-01-09 17:01:28 +0900

    Test: 프래그먼트 전환 테스트 코드 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,6 +21,8 @@ android {
         multiDexEnabled = true
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = "com.example.egobook_frontent.HiltTestRunner"
+
     }
 
     buildTypes {
@@ -73,4 +75,12 @@ dependencies {
 
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
     implementation("com.kizitonwose.calendar:view:2.6.0")
+
+    // Hilt 테스트를 위한 의존성 추가
+    androidTestImplementation("com.google.dagger:hilt-android-testing:2.51.1")
+    kaptAndroidTest("com.google.dagger:hilt-android-compiler:2.51.1")
+}
+
+kapt {
+    correctErrorTypes = true
 }
