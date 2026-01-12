1546dbe8e8ea2d736d6a84bbe1f30ea50827ceca
princehw03 <princehw03@gmail.com>
2026-01-12 23:32:53 +0900

    feat: 마지막 온보딩화면에서 홈프래그먼트로의 전환 기능 구현

--- app/src/main/java/com/example/egobook_frontent/ui/onboarding/view/OnboardingFragment5.kt
+++ app/src/main/java/com/example/egobook_frontent/ui/onboarding/view/OnboardingFragment5.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.egobook_frontent.R
 import com.example.egobook_frontent.databinding.FragmentOnboarding5Binding
 
 class OnboardingFragment5 : Fragment() {
@@ -18,6 +20,14 @@ class OnboardingFragment5 : Fragment() {
     ): View {
         _binding = FragmentOnboarding5Binding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnStartEgobook.setOnClickListener {
+            findNavController().navigate(R.id.action_onboarding_to_home)
+        }
     }
 
     override fun onDestroyView() {
