0d2e9e8a0a35a86f3f8f1eaec610a53f6008003e
se05503 <seyeongb18@gmail.com>
2026-01-12 17:44:11 +0900

    test(counseling): 임시 테스트 용 mock 데이터 추가 - 백엔드 API 연결 전 repositoryImpl 에서 mock 데이터로 테스트 - 백엔드 연결 후 주석 코드 해제 필요 (나중에 재커밋할 예정)

--- app/src/main/java/com/example/egobook_frontent/data/repository/CounselingRepositoryImpl.kt
+++ app/src/main/java/com/example/egobook_frontent/data/repository/CounselingRepositoryImpl.kt
@@ -91,24 +91,26 @@ class CounselingRepositoryImpl @Inject constructor(private val apiService: Couns
     }
 
     override suspend fun getWeeklyReportStyle(): Result<WeeklyReportStyle> = try {
-        val response = apiService.fetchWeeklyReportStyle()
-        if(response.isSuccessful && response.body() != null) {
-            Result.success(response.body()!!.toDomain())
-        } else {
-            Result.failure(Exception("Error: ${response.code()}"))
-        }
+//        val response = apiService.fetchWeeklyReportStyle()
+//        if(response.isSuccessful && response.body() != null) {
+//            Result.success(response.body()!!.toDomain())
+//        } else {
+//            Result.failure(Exception("Error: ${response.code()}"))
+//        }
+        Result.success(WeeklyReportStyle(type = ReportStyle.SOFT))
     } catch (e: Exception) {
         Result.failure(e)
     }
 
 
     override suspend fun updateWeeklyReportStyle(reportStyle: ReportStyle): Result<ReportStyle> = try {
-        val response = apiService.updateWeeklyReportStyle(reportStyle = reportStyle)
-        if(response.isSuccessful) {
-            Result.success(reportStyle)
-        } else {
-            Result.failure(Exception("Error: ${response.code()}"))
-        }
+//        val response = apiService.updateWeeklyReportStyle(reportStyle = reportStyle)
+//        if(response.isSuccessful) {
+//            Result.success(reportStyle)
+//        } else {
+//            Result.failure(Exception("Error: ${response.code()}"))
+//        }
+        Result.success(reportStyle)
     } catch (e: Exception) {
         Result.failure(e)
     }
