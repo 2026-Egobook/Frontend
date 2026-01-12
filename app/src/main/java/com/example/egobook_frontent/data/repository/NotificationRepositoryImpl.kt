076cb522b4c8f4e86179da0d29fb903795bcc86e
se05503 <seyeongb18@gmail.com>
2026-01-12 17:44:25 +0900

    test(notification): 임시 테스트 용 mock 데이터 추가 - 백엔드 API 연결 전 repositoryImpl 에서 mock 데이터로 테스트 - 백엔드 연결 후 주석 코드 해제 필요 (나중에 재커밋할 예정)

--- app/src/main/java/com/example/egobook_frontent/data/repository/NotificationRepositoryImpl.kt
+++ app/src/main/java/com/example/egobook_frontent/data/repository/NotificationRepositoryImpl.kt
@@ -9,23 +9,25 @@ import javax.inject.Inject
 
 class NotificationRepositoryImpl @Inject constructor(private val apiService: NotificationService): NotificationRepository {
     override suspend fun getNotificationStatus(): Result<Notification> = try {
-        val response = apiService.getNotificationStatus()
-        if(response.isSuccessful && response.body() != null) {
-            Result.success(response.body()!!.toDomain())
-        } else {
-            Result.failure(Exception("Error: ${response.code()}"))
-        }
+//        val response = apiService.getNotificationStatus()
+//        if(response.isSuccessful && response.body() != null) {
+//            Result.success(response.body()!!.toDomain())
+//        } else {
+//            Result.failure(Exception("Error: ${response.code()}"))
+//        }
+        Result.success(Notification(isDailyPraiseEnabled = true, isWeeklyReportEnabled = true))
     } catch (e: Exception) {
         Result.failure(e)
     }
 
     override suspend fun updateNotificationStatus(type: NotificationType, isEnabled: Boolean): Result<Boolean> = try {
-        val response = apiService.updateNotificationStatus(type = type, isEnabled = isEnabled)
-        if(response.isSuccessful) {
-            Result.success(isEnabled)
-        } else {
-            Result.failure(Exception("Error: ${response.code()}"))
-        }
+//        val response = apiService.updateNotificationStatus(type = type, isEnabled = isEnabled)
+//        if(response.isSuccessful) {
+//            Result.success(isEnabled)
+//        } else {
+//            Result.failure(Exception("Error: ${response.code()}"))
+//        }
+        Result.success(isEnabled)
     } catch (e: Exception) {
         Result.failure(e)
     }
