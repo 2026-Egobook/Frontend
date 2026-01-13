ee27c7999b5f7dc7642e1a68f15faf0884f10feb
aksworns22 <wkdeogks17@gmail.com>
2026-01-13 18:54:34 +0900

    feat(StoreFragment): 상점 프래그먼트 추가

---
+++ app/src/main/java/com/example/egobook_frontent/ui/shop/StoreFragment.kt
@@ -0,0 +1,7 @@
+package com.example.egobook_frontent.ui.shop
+
+import androidx.fragment.app.Fragment
+import com.example.egobook_frontent.R
+
+class StoreFragment: Fragment(R.layout.fragment_store) {
+}
