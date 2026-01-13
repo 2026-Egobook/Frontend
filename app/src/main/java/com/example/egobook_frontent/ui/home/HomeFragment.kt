99b99a7915947d979ba739ea46e91bfc214350b7
aksworns22 <wkdeogks17@gmail.com>
2026-01-13 16:35:28 +0900

    feat(HomeViewModel): 사용자 상태 관리 구현

--- app/src/main/java/com/example/egobook_frontent/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/egobook_frontent/ui/home/HomeFragment.kt
@@ -1,7 +1,39 @@
 package com.example.egobook_frontent.ui.home
 
-import androidx.fragment.app.Fragment
-import com.example.egobook_frontent.R
+import android.os.Bundle
 
-class HomeFragment : Fragment(R.layout.fragment_home) {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.example.egobook_frontent.databinding.FragmentHomeBinding
+import kotlinx.coroutines.launch
+
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val viewModel: HomeViewModel by viewModels()
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.collect { userState ->
+                    binding.tvLevel.text = "Lv ${userState.level.number}"
+                    binding.ivLevelType.setImageResource(userState.level.type.badgeUiId)
+                    binding.tvInk.text = "${userState.ink.value}"
+                }
+            }
+        }
+    }
 }
