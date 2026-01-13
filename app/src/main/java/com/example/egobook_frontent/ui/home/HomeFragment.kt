1c499f6fcc7d58799ff530a7dc2a974948c58147
aksworns22 <wkdeogks17@gmail.com>
2026-01-13 18:54:53 +0900

    feat: HomeFragment에서 상점 이동 구현

--- app/src/main/java/com/example/egobook_frontent/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/egobook_frontent/ui/home/HomeFragment.kt
@@ -1,7 +1,30 @@
 package com.example.egobook_frontent.ui.home
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.egobook_frontent.R
+import com.example.egobook_frontent.databinding.FragmentHomeBinding
+import com.example.egobook_frontent.ui.shop.StoreFragment
 
-class HomeFragment : Fragment(R.layout.fragment_home) {
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.ivStore.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_storeFragment)
+        }
+    }
 }
