97edb280a1d657417cbeea086619fb659bb6174f
aksworns22 <wkdeogks17@gmail.com>
2026-01-15 16:53:28 +0900

    feat(store): 상점 탭 전환 구현

--- app/src/main/java/com/example/egobook_frontent/ui/shop/StoreFragment.kt
+++ app/src/main/java/com/example/egobook_frontent/ui/shop/StoreFragment.kt
@@ -1,7 +1,43 @@
 package com.example.egobook_frontent.ui.shop
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.egobook_frontent.R
+import androidx.viewpager2.widget.ViewPager2
+import com.example.egobook_frontent.databinding.FragmentStoreBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
-class StoreFragment: Fragment(R.layout.fragment_store) {
+class StoreFragment: Fragment() {
+    private var _binding: FragmentStoreBinding? = null
+    private val binding get() = checkNotNull(_binding) { "Fragment가 제거되었습니다." }
+    private lateinit var viewPager: ViewPager2
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentStoreBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewPager = binding.vp2StoreCollectionContainer
+        viewPager.adapter = StoreCollectionAdapter(this)
+        val tabLayout = binding.tabLayout
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = ItemTab.of(position).text
+        }.attach()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
+
+
+
