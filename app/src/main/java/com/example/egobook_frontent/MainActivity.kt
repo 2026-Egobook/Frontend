b9022d14fd661e041cde7bb53337c2b13a4019be
aksworns22 <wkdeogks17@gmail.com>
2026-01-18 18:33:58 +0900

    feat(MainActivity): 상점 탭에서 하단 네비게이션 숨기기

--- app/src/main/java/com/example/egobook_frontent/MainActivity.kt
+++ app/src/main/java/com/example/egobook_frontent/MainActivity.kt
@@ -38,7 +38,8 @@ class MainActivity : AppCompatActivity() {
             when (destination.id) {
                 R.id.onboardingContainerFragment, // 온보딩 화면
                 R.id.diaryWriteFragment,        // 일기 작성 화면
-                R.id.candlerFragment            // 달력 화면
+                R.id.candlerFragment, // 달력 화면
+                R.id.storeFragment,
                     -> {
                     binding.bottomNavigation.visibility = View.GONE
                 }
