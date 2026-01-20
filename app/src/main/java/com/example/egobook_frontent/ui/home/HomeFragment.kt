a7c4d172b2dfb900ad9e4d0509911bca779957b3
aksworns22 <wkdeogks17@gmail.com>
2026-01-20 20:17:06 +0900

    feat: 나의 성향 다이어로그 ui 구현

--- app/src/main/java/com/example/egobook_frontent/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/egobook_frontent/ui/home/HomeFragment.kt
@@ -49,6 +49,13 @@ class HomeFragment : Fragment() {
             dialog.isCancelable = false
             dialog.show(parentFragmentManager, "ConfirmDialog")
         }
+
+        binding.ivRadar.setOnClickListener {
+            applyScreenBlur(BlurLevel.BASE)
+            val dialog = RadarDialog()
+            dialog.isCancelable = false
+            dialog.show(parentFragmentManager, "RadarDialog")
+        }
     }
 
     private fun LevelType.getResId(): Int {
