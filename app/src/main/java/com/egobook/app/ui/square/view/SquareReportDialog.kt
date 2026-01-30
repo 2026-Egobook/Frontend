3030f520fd00956079841e899296334005e2d7be
se05503 <seyeongb18@gmail.com>
2026-01-30 15:20:45 +0900

    feat(question): 신고하기 클릭 이벤트 - 각 항목 클릭 시 UI 변경 - 신고하기 버튼 활성화 유무 표시

--- app/src/main/java/com/egobook/app/ui/square/view/SquareReportDialog.kt
+++ app/src/main/java/com/egobook/app/ui/square/view/SquareReportDialog.kt
@@ -3,8 +3,11 @@ package com.egobook.app.ui.square.view
 import android.app.Dialog
 import android.graphics.Color
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import androidx.core.graphics.drawable.toDrawable
+import androidx.core.view.isVisible
 import androidx.fragment.app.DialogFragment
 import com.egobook.app.R
 import com.egobook.app.databinding.DialogSquareReportBinding
@@ -12,6 +15,9 @@ import com.egobook.app.removeScreenBlur
 
 class SquareReportDialog: DialogFragment(R.layout.dialog_square_report) {
     private lateinit var binding: DialogSquareReportBinding
+    private val clickContentList by lazy {
+        listOf(binding.tvSquareReportAbuse, binding.tvSquareReportSpam, binding.tvSquareReportInappropriate, binding.tvSquareReportEtcNotClicked)
+    }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return Dialog(requireContext()).apply {
@@ -30,6 +36,36 @@ class SquareReportDialog: DialogFragment(R.layout.dialog_square_report) {
             removeScreenBlur()
             dismiss()
         }
+        clickContentList.forEach { content ->
+            content.setOnClickListener {
+                clickContentList.forEach { it.isSelected = false }
+                content.isSelected = true
+                if(tvSquareReportEtcNotClicked.isSelected) {
+                    llSquareReportEtcClicked.isVisible = true
+                    tvSquareReportEtcNotClicked.isVisible = false
+                } else {
+                    llSquareReportEtcClicked.isVisible = false
+                    tvSquareReportEtcNotClicked.isVisible = true
+                }
+            }
+        }
+        etSquareReportEtcReason.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(p0: Editable?) = Unit
+            override fun beforeTextChanged(
+                p0: CharSequence?,
+                p1: Int,
+                p2: Int,
+                p3: Int
+            ) = Unit
+            override fun onTextChanged(
+                text: CharSequence?,
+                start: Int,
+                before: Int,
+                count: Int
+            ) {
+                btnSquareReportSubmit.isEnabled = !text.isNullOrBlank()
+            }
+        })
     }
 
     companion object {
