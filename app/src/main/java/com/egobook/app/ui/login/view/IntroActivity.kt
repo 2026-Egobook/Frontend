47baa32a74ea45cc2d0a04c4c43197d69f380cf9
princehw03 <princehw03@gmail.com>
2026-02-04 17:05:17 +0900

    refactor: 스플래시 로직 리팩토링: 일단은 배경색만 출력되게 변경

---
+++ app/src/main/java/com/egobook/app/ui/login/view/IntroActivity.kt
@@ -0,0 +1,25 @@
+package com.egobook.app.ui.login.view
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.egobook.app.R
+import com.egobook.app.data.local.UserInfoStorage
+import javax.inject.Inject
+
+class IntroActivity : AppCompatActivity() {
+    @Inject lateinit var userInfoStorage: UserInfoStorage
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_intro)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
