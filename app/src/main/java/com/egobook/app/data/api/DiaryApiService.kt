cecb355dd12c82e4aba5b1e54f9e41b228d96eb3
princehw03 <princehw03@gmail.com>
2026-02-07 01:22:12 +0900

    fix: 갑자기 코드에 전부 빨간색줄이 뜨는 오류 해결

--- app/src/main/java/com/egobook/app/data/api/DiaryApiService.kt
+++ app/src/main/java/com/egobook/app/data/api/DiaryApiService.kt
@@ -1,5 +1,20 @@
 package com.egobook.app.data.api
 
+import com.egobook.app.data.model.ApiResponse
+import com.egobook.app.data.model.diary.response.DiariesResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface DiaryApiService {
+
+    //일기 목록 불러오기
+    @GET("/diaries")
+    suspend fun getDiaries(
+        @Query("date") date: String,
+        @Query("type") type: String,
+        @Query("page") page: Int = 1,
+        @Query("size") size: Int = 10
+    ): ApiResponse<DiariesResponse>
+
 
 }
