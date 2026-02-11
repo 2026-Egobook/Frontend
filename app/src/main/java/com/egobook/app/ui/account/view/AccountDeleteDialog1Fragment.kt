7b952742254a36dbb678a3a6928eed8f46c0eefb
princehw03 <princehw03@gmail.com>
2026-02-11 17:55:25 +0900

    feat: 2번째 dialog후 로그인 화면 이동 로직 구현

--- app/src/main/java/com/egobook/app/ui/account/view/AccountDeleteDialog1Fragment.kt
+++ app/src/main/java/com/egobook/app/ui/account/view/AccountDeleteDialog1Fragment.kt
@@ -7,13 +7,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.viewModels
 import com.egobook.app.databinding.FragmentAccountDeleteDialog1Binding
 import com.egobook.app.removeScreenBlur
+import com.egobook.app.ui.account.viewmodel.AccountViewModel
+import kotlin.getValue
 
 class AccountDeleteDialog1Fragment : DialogFragment() {
 
     private var _binding: FragmentAccountDeleteDialog1Binding? = null
     private val binding get() = _binding!!
+
+    //부모 프래그먼트의 뷰모델 공유
+    private val viewModel: AccountViewModel by viewModels({ requireParentFragment() })
 
     override fun onCreateView(
         inflater: LayoutInflater,
--- app/src/main/java/com/egobook/app/ui/account/view/AccountDeleteDialog2Fragment.kt
+++ app/src/main/java/com/egobook/app/ui/account/view/AccountDeleteDialog2Fragment.kt
@@ -1,6 +1,7 @@
 package com.egobook.app.ui.account.view
 
 import android.content.DialogInterface
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
@@ -9,13 +10,22 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.egobook.app.databinding.FragmentAccountDeleteDialog2Binding
 import com.egobook.app.removeScreenBlur
+import com.egobook.app.ui.account.viewmodel.AccountViewModel
+import com.egobook.app.ui.login.view.LoginActivity
+import timber.log.Timber
+import kotlin.getValue
 
 class AccountDeleteDialog2Fragment : DialogFragment() {
 
     private var _binding: FragmentAccountDeleteDialog2Binding? = null
     private val binding get() = _binding!!
+
+    //부모 프래그먼트의 뷰모델 공유
+    private val viewModel: AccountViewModel by viewModels({ requireParentFragment() })
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,8 +42,17 @@ class AccountDeleteDialog2Fragment : DialogFragment() {
         // TODO: 뷰 초기화 로직 추가
     }
 
+
     override fun onCancel(dialog: DialogInterface) {
         super.onCancel(dialog)
+
+        //백스택 제거 후 로그인 화면으로 이동
+        val intent = Intent(context, LoginActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        startActivity(intent)
+        Timber.d("로그인 화면으로 이동")
+
         removeScreenBlur()
     }
 
@@ -41,4 +60,6 @@ class AccountDeleteDialog2Fragment : DialogFragment() {
         super.onDestroyView()
         _binding = null
     }
+
+
 }
