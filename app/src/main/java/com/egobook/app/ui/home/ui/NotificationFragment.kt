736ce328b2c8aca6393f93ddc21f12363451c6f8
aksworns22 <wkdeogks17@gmail.com>
2026-02-12 15:10:38 +0900

    feat: 알림에 해당하는 위치로 이동

--- app/src/main/java/com/egobook/app/ui/home/ui/NotificationFragment.kt
+++ app/src/main/java/com/egobook/app/ui/home/ui/NotificationFragment.kt
@@ -10,10 +10,13 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.egobook.app.R
 import com.egobook.app.databinding.FragmentNotificationBinding
 import com.egobook.app.ui.home.NotificationViewModel
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -22,7 +25,12 @@ class NotificationFragment: Fragment() {
     private var _binding: FragmentNotificationBinding? = null
     private val binding get() = checkNotNull(_binding) { "Fragment가 제거되었습니다." }
     private val viewModel: NotificationViewModel by viewModels()
-    private val notificationAdapter = NotificationAdapter()
+    private val notificationAdapter = NotificationAdapter { notification ->
+        val bottomNav = requireActivity().findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        // "EgoRoom 탭 버튼을 누른 것으로 처리해줘!"
+        bottomNav.selectedItemId = R.id.menu_ego_room
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
