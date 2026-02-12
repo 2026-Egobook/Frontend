93ae592624d2fdb158326751e4b0f1d5918db8ca
se05503 <seyeongb18@gmail.com>
2026-02-12 21:10:16 +0900

    refactor

--- app/src/main/java/com/egobook/app/data/repository/CounselingRepositoryImpl.kt
+++ app/src/main/java/com/egobook/app/data/repository/CounselingRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.egobook.app.data.repository
 
+import android.util.Log
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
--- app/src/main/java/com/egobook/app/ui/square/view/SquareAllRepliesFragment.kt
+++ app/src/main/java/com/egobook/app/ui/square/view/SquareAllRepliesFragment.kt
@@ -2,6 +2,7 @@ package com.egobook.app.ui.square.view
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Lifecycle
@@ -9,6 +10,7 @@ import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import androidx.paging.LoadState
 import com.egobook.app.BlurLevel
 import com.egobook.app.R
 import com.egobook.app.applyScreenBlur
@@ -59,8 +61,18 @@ class SquareAllRepliesFragment : Fragment(R.layout.fragment_square_all_replies) 
     private fun initObservers() = with(binding) {
         viewLifecycleOwner.lifecycleScope.launch {
             viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.todayAllUserReplies.collectLatest { pagingData ->
-                    if(pagingData != null) adapter.submitData(lifecycle, pagingData)
+                launch {
+                    viewModel.todayAllUserReplies.collectLatest { pagingData ->
+                        if(pagingData != null) adapter.submitData(lifecycle, pagingData)
+                    }
+                }
+                launch {
+                    adapter.loadStateFlow.collectLatest { loadStates ->
+                        val isRefreshing = loadStates.refresh is LoadState.Loading
+                        val isListEmpty = loadStates.refresh is LoadState.NotLoading && adapter.itemCount == 0
+                        tvSquareAllRepliesPlaceholder.isVisible = isListEmpty
+                        rvSquareAllReplies.isVisible = !isListEmpty
+                    }
                 }
             }
         }
