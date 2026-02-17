f226611520f6e93b372d9b347f76e910f2778402
princehw03 <princehw03@gmail.com>
2026-02-17 22:21:58 +0900

    refactor: authError 메세지 수정

--- app/src/main/java/com/egobook/app/domain/model/auth/AuthError.kt
+++ app/src/main/java/com/egobook/app/domain/model/auth/AuthError.kt
@@ -4,24 +4,31 @@ sealed class AuthError(
     override val message: String? = null
 ) : Throwable(message) {
 
+    //400
     class BadRequest :
         AuthError("잘못된 요청입니다.")
 
+    //401
     class InvalidCredentials :
-        AuthError("유효하지 않은 구글 계정입니다.")
+        AuthError("유효하지 않은 계정입니다.")
 
+    //403
     class WaitDelete :
         AuthError("탈퇴 처리 중인 계정입니다. 관리자에게 문의하세요.")
 
+    //409
     class UserAlreadyExists :
-        AuthError("이미 가입된 계정입니다.")
+        AuthError("이미 가입된 구글 계정입니다.")
 
+    //404
     class UserNotFound :
         AuthError("존재하지 않는 사용자입니다.")
 
+    //미정
     class NetworkError :
         AuthError("네트워크 오류가 발생했습니다. 잠시 후 다시 시도해주세요.")
 
+    //else
     class Unknown(message: String?) :
         AuthError(message)
 }
