dacdcc049f2adfbe4b154faa56d7af1de6ffa996
2godong <sora050821@gmail.com>
2026-05-05 20:52:19 +0900

    🔨 refactor:  .ginore google 파일 추가

--- app/src/main/java/com/egobook/app/ui/login/view/LoginActivity.kt
+++ app/src/main/java/com/egobook/app/ui/login/view/LoginActivity.kt
@@ -113,8 +113,9 @@ import javax.inject.Inject
 
             //게스트 로그인 버튼 클릭이벤트
             binding.btnGuestLogin.setOnClickListener {
-// 임시 주석               viewModel.onEvent(LoginEvent.TryGuestLogin)
-                navigateToOnboarding()
+             viewModel.onEvent(LoginEvent.TryGuestLogin)
+//                임시 주석
+//                navigateToOnboarding()
             }
 
             // Google 계정으로 회원가입 버튼 - 구글 로그인 창 띄우기
